3d9c6f2346356bb4f3f78d5ec5aad37eefa2a53a
Hugo Piedrafita <xdhugoxd37@gmail.com>
2025-11-21 16:44:46 +0100

    Update storage.py

--- app/models/storage.py
+++ app/models/storage.py
@@ -1,1 +1,14 @@
+# Almacenamiento temporal en memoria
+products = []
+next_id = 1
+
+def insert_product(product):
+    global next_id
+    product.id = next_id
+    next_id += 1
+    products.append(product)
+    return product
+
+def get_all_products():
+    return products
 
